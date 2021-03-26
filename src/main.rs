e36f4ceb2a10170df1810f9100cb249fad674569
Wybe Westra <wybe@ruurdwestra.nl>
2021-03-26 19:24:35 +0100

    + Basic vulkan testing.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use vulkano::instance::{Instance, InstanceExtensions, PhysicalDevice};
+
 fn main() {
     println!("Hello, world!");
+
+    let instance =
+        Instance::new(None, &InstanceExtensions::none(), None).expect("Failed to create instance.");
+
+    let physical = PhysicalDevice::enumerate(&instance)
+        .next()
+        .expect("No device available");
+
+    // Debug print all the queue families.
+    for family in physical.queue_families() {
+        println!(
+            "Found a queue family with {:?} queues. Supports graphics: {:?}, compute: {:?}",
+            family.queues_count(),
+            family.supports_graphics(),
+            family.supports_compute()
+        );
+    }
 }

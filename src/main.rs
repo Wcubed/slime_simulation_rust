5ed571bde2fd08b917784de023d83f9c2aa3e42c
Wybe Westra <wybe@ruurdwestra.nl>
2021-03-27 12:03:45 +0100

    + Compute shader works!

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,12 @@ use crate::system::System;
 use imgui::{im_str, Condition, Window};
 use std::sync::Arc;
 use vulkano::buffer::{BufferUsage, CpuAccessibleBuffer};
+use vulkano::command_buffer::{AutoCommandBufferBuilder, CommandBuffer};
+use vulkano::descriptor::descriptor_set::PersistentDescriptorSet;
+use vulkano::descriptor::pipeline_layout::PipelineLayoutDesc;
+use vulkano::descriptor::PipelineLayoutAbstract;
 use vulkano::pipeline::ComputePipeline;
+use vulkano::sync::GpuFuture;
 
 mod system;
 
@@ -11,6 +16,7 @@ fn main() {
 
     let system = System::init("Slime Simulation");
 
+    // Compute shader commands are taken from here: https://vulkano.rs/guide/compute-intro
     let data_iter = 0..65536;
     let data_buffer =
         CpuAccessibleBuffer::from_iter(system.device.clone(), BufferUsage::all(), false, data_iter)
@@ -22,6 +28,34 @@ fn main() {
         ComputePipeline::new(system.device.clone(), &shader.main_entry_point(), &(), None)
             .expect("Failed to create compute pipeline"),
     );
+
+    let layout = compute_pipeline.layout().descriptor_set_layout(0).unwrap();
+    let set = Arc::new(
+        PersistentDescriptorSet::start(layout.clone())
+            .add_buffer(data_buffer.clone())
+            .unwrap()
+            .build()
+            .unwrap(),
+    );
+
+    let mut builder =
+        AutoCommandBufferBuilder::new(system.device.clone(), system.queue.family()).unwrap();
+    builder
+        .dispatch([1024, 1, 1], compute_pipeline.clone(), set.clone(), ())
+        .unwrap();
+    let command_buffer = builder.build().unwrap();
+
+    let finished = command_buffer.execute(system.queue.clone()).unwrap();
+    finished
+        .then_signal_fence_and_flush()
+        .unwrap()
+        .wait(None)
+        .unwrap();
+
+    let content = data_buffer.read().unwrap();
+    for (n, val) in content.iter().enumerate() {
+        println!("{}: {}", n, val);
+    }
 
     system.main_loop(move |_, ui| {
         Window::new(im_str!("Hello World!"))
